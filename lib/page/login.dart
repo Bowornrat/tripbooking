614b39223a1f28a6bc8381532decbc6f90953b43
Bowornrat <bowornrats@gmail.com>
2023-05-29 14:34:32 +0700

    .

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -6,47 +6,50 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 100.0),
-              child: Image.asset(
-                'assets/images/logo.jpg',
-                width: 250,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 100.0),
+                child: Image.asset(
+                  'assets/images/logo.jpg',
+                  width: 250,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 50.0),
-            child: const Text('หมายเลขโทรศัพท์'),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 50.0, right: 50),
-            child: TextField(),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0, left: 50),
-            child: const Text('รหัสผ่าน'),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 50.0, right: 50),
-            child: TextField(),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                    onPressed: () {}, child: const Text('ลงทะเบียนใหม่')),
-                FilledButton(onPressed: () {}, child: const Text('เข้าสู่ระบบ'))
-              ],
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0),
+              child: const Text('หมายเลขโทรศัพท์'),
             ),
-          )
-        ],
+            const Padding(
+              padding: EdgeInsets.only(left: 50.0, right: 50),
+              child: TextField(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, left: 50),
+              child: const Text('รหัสผ่าน'),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 50.0, right: 50),
+              child: TextField(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: const Text('ลงทะเบียนใหม่')),
+                  FilledButton(
+                      onPressed: () {}, child: const Text('เข้าสู่ระบบ'))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
     ;

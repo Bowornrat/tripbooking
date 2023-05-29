b79df80dc0317cb99763d4308b6ff79d530c21e0
Bowornrat <bowornrats@gmail.com>
2023-05-29 15:29:28 +0700

    .

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -2,11 +2,23 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Attribute or Property
+  // Global variable
+  String myText1 = '';
+
+  @override
   Widget build(BuildContext context) {
+    // Local variable
+    String myText2 = '';
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -22,17 +34,17 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 50.0),
-              child: const Text('หมายเลขโทรศัพท์'),
+            const Padding(
+              padding: EdgeInsets.only(left: 50.0),
+              child: Text('หมายเลขโทรศัพท์'),
             ),
             const Padding(
               padding: EdgeInsets.only(left: 50.0, right: 50),
               child: TextField(),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0, left: 50),
-              child: const Text('รหัสผ่าน'),
+            const Padding(
+              padding: EdgeInsets.only(top: 16.0, left: 50),
+              child: Text('รหัสผ่าน'),
             ),
             const Padding(
               padding: EdgeInsets.only(left: 50.0, right: 50),
@@ -47,20 +59,27 @@ class LoginPage extends StatelessWidget {
                       onPressed: () {}, child: const Text('ลงทะเบียนใหม่')),
                   FilledButton(
                       onPressed: () {
-                        // 1.
-                        print('Hello World!!!!');
+                        // // 1.
+                        // print('Hello World!!!!');
 
-                        // 2.
-                        // must import 'dart:developer';
-                        log('Hello World!!!!');
+                        // // 2.
+                        // // must import 'dart:developer';
+                        // log('Hello World!!!!');
 
-                        // 3.
-                        debugPrint('Hello World!!!!');
+                        // // 3.
+                        // debugPrint('Hello World!!!!');
+
+                        // Render UI
+                        setState(() {
+                          myText1 = 'Hellooooooooooooo!';
+                          log(myText1);
+                        });
                       },
                       child: const Text('เข้าสู่ระบบ'))
                 ],
               ),
-            )
+            ),
+            Center(child: Text(myText1))
           ],
         ),
       ),

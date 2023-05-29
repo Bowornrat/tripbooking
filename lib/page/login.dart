46c4ef4e4b6d2aa8c7a7e40b425d3e8de245500c
Bowornrat <bowornrats@gmail.com>
2023-05-29 13:37:34 +0700

    ี.

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -6,10 +6,21 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      body: Column(
+        children: [
+          Image.network('https://d1yjjnpx0p53s8.cloudfront.net/logo_173_0.jpg'),
+          const Text('หมายเลขโทรศัพท์'),
+          TextField(),
+          const Text('รหัสผ่าน'),
+          TextField(),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: const Text('ลงทะเบียนใหม่')),
+              FilledButton(onPressed: () {}, child: const Text('เข้าสู่ระบบ'))
+            ],
+          )
+        ],
       ),
-      body: Container(),
     );
     ;
   }

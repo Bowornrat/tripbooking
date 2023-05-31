b2841f13a2967aaa3d6c95ee696de59702a73ea1
Bowornrat <bowornrats@gmail.com>
2023-05-31 15:15:02 +0700

    .

--- lib/page/tripdetail.dart
+++ lib/page/tripdetail.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:tripbooking/config.dart';
+import 'package:tripbooking/model/trip_get_res.dart';
 
 class TripDetailPage extends StatefulWidget {
   int idx = 0;
@@ -21,5 +24,17 @@ class _TripDetailPageState extends State<TripDetailPage> {
       ),
       body: Container(),
     );
+  }
+
+  // Method สำหรับโหลดข้อมูลที่เป็น Async
+  Future<void> loadDataAsync() async {
+    // 1. Gen url
+    var url = '$apiEndpoint/trip?filter=idx,eq,${widget.idx}';
+
+    // 2. Call Api
+    var response = await http.get(Uri.parse(url));
+
+    // 3. Convert JSON to model
+    var trip = tripGetResponseFromJson(response.body);
   }
 }

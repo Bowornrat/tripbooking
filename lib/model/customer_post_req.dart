9640449afca03632c0481f5a8fe714d2a49b6fcb
Bowornrat <bowornrats@gmail.com>
2023-05-31 09:41:55 +0700

    .

---
+++ lib/model/customer_post_req.dart
@@ -0,0 +1,48 @@
+// To parse this JSON data, do
+//
+//     final customerPostRequest = customerPostRequestFromJson(jsonString);
+
+import 'dart:convert';
+
+CustomerPostRequest customerPostRequestFromJson(String str) =>
+    CustomerPostRequest.fromJson(json.decode(str));
+
+String customerPostRequestToJson(CustomerPostRequest data) =>
+    json.encode(data.toJson());
+
+class CustomerPostRequest {
+  int idx;
+  String fullname;
+  String phone;
+  String email;
+  String image;
+  String password;
+
+  CustomerPostRequest({
+    required this.idx,
+    required this.fullname,
+    required this.phone,
+    required this.email,
+    required this.image,
+    required this.password,
+  });
+
+  factory CustomerPostRequest.fromJson(Map<String, dynamic> json) =>
+      CustomerPostRequest(
+        idx: json["idx"],
+        fullname: json["fullname"],
+        phone: json["phone"],
+        email: json["email"],
+        image: json["image"],
+        password: json["password"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "idx": idx,
+        "fullname": fullname,
+        "phone": phone,
+        "email": email,
+        "image": image,
+        "password": password,
+      };
+}

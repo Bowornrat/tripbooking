b8e26ebbe24f98d29f54135c1b81f7cc961a6658
Bowornrat <bowornrats@gmail.com>
2023-06-01 15:25:23 +0700

    .

--- lib/page/maps.dart
+++ lib/page/maps.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,12 +17,15 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         title: Text('แผนที่'),
       ),
-      body: const GoogleMap(
-        mapType: MapType.hybrid,
+      body: GoogleMap(
+        mapType: MapType.normal,
         initialCameraPosition: CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 14.4746,
+          target: LatLng(17.294602111982684, 104.11267828243643),
+          zoom: 18,
         ),
+        onCameraMove: (position) {
+          log('${position.target.latitude} ${position.target.longitude}');
+        },
       ),
     );
   }
